e940d03416994874d3b1a9b87f8f2f17f2c7f7d6
AmanShukla111 <shuklaaman99520@gmail.com>
2024-11-30 21:21:26 +0530

    Added devise support.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :forms do
     resources :form_fields, only: [:new, :create, :edit, :update, :destroy]
     resources :responses
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_29_160836) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_30_135804) do
   create_table "answers", force: :cascade do |t|
     t.integer "response_id", null: false
     t.integer "form_field_id", null: false
@@ -43,6 +43,18 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_29_160836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["form_id"], name: "index_responses_on_form_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "answers", "form_fields"
